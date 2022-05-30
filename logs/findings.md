7780fb5c202640f02d42de7bdf72ba7702590ff5
Charles Kumar <gamescharlie79@gmail.com>
2022-05-30 06:41:57 +0530

    sender email configured with mailhog, fortify email verification and user role attached in CreateNewUsers

---
+++ logs/findings.md
@@ -0,0 +1,3 @@
+https://www.telerik.com/blogs/manipulating-colors-with-color-picker-in-chrome-devtools (kendo UI)
+https://convertingcolors.com/rgb-color-125_211_252.html?search=rgb(125%20211%20252)
+https://hunter.io/blog/how-to-verify-email-address/  Irina Maltseva entertain my cat Persie and collect airline miles.
--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -17,17 +17,17 @@
         <script src="{{ asset('js/app.js') }}" defer></script>
     </head>
     <body class="font-sans antialiased">
-        <div class="min-h-screen bg-gray-100">
+        <div class="min-h-screen bg-slate-200">
             @include('layouts.navigation')
 
             <!-- Page Heading -->
-            <header class="bg-white shadow">
+            {{-- <header class="bg-white shadow">
                 <div class="max-w-7xl mx-auto py-6 px-4 sm:px-6 lg:px-8">
                     @isset($header)
                         {{ $header }}
                     @endisset
                 </div>
-            </header>
+            </header> --}}
 
             <!-- Page Content -->
             <main>
